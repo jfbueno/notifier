f1c915162daea93f13bad7c058b736434240ff0c
Jéferson <buenojeferson@outlook.com>
2017-07-05 18:18:21 -0300

    alterações visuais

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+const defaults = {
+    title: 'Olar!',
+    message: 'Não esqueça de beber água!',
+    timeout: 1 * 60 * 60 * 1000
+}
+
 window.onload = function() {
     if(Notification.permission !== "granted") {    
-        Notification.requestPermission().then(function(result) {
+        /*Notification.requestPermission().then(function(result) {
             console.log(result);
-        });
+        });*/
     }
 
     notify();
@@ -10,11 +16,11 @@ window.onload = function() {
 
 function notify() {
     var options = {
-        body: 'Não esquecer de beber água.',
+        body: defaults.message,
         icon: 'img/squirtle.png'
     }
   
-    var notification = new Notification("Olar!", options);
+    var notification = new Notification(defaults.title, options);
 
     setTimeout(notify, 1 * 60 * 60 * 1000);
 }
