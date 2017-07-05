a8d76c4c58de88bf21a60b164daee539c43da5b0
Jéferson <buenojeferson@outlook.com>
2017-07-05 14:30:25 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+    if(Notification.permission !== "granted") {    
+        Notification.requestPermission().then(function(result) {
+            console.log(result);
+        });
+    }
+}
+
+function notify() {
+    var options = {
+        body: 'Não esquecer de beber água.',
+        icon: 'img/squirtle.png'
+    }
+  
+    var notification = new Notification("Olar!", options);
+
+    setTimeout(notify, 1 * 60 * 60 * 1000);
+}
