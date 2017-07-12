933daefc9a2c6e6f75bdc8244158706cc7a7ebfe
Jéferson <buenojeferson@outlook.com>
2017-07-12 18:20:56 -0300

    Mensagens configuráveis

--- script.js
+++ script.js
@@ -1,26 +1,60 @@
 const defaults = {
     title: 'Olar!',
-    message: 'Não esqueça de beber água!',
-    timeout: 1 * 60 * 60 * 1000
+    body: 'Não esqueça de beber água!',
+    minutesTimeout: 15
 }
+
+const CONFIG_LOCAL_STORAGE_KEY = '';
 
 window.onload = function() {
     if(Notification.permission !== "granted") {    
-        /*Notification.requestPermission().then(function(result) {
+        Notification.requestPermission().then(function(result) {
             console.log(result);
-        });*/
+        });
     }
 
+    __init();
+}
+
+function __init() {    
+    document.getElementById('btn-config').addEventListener('click', btnConfigClick);    
+    initComponents();
     notify();
 }
 
+function initComponents() {
+    const config = getConfigObj();
+
+    for(let prop in config){
+        document.getElementsByName(prop)[0].value = config[prop] || defaults[prop];
+    }
+}
+
+function btnConfigClick() {
+    const configObj = { }
+    const inputs = document.querySelectorAll('#config-container>input');
+
+    for(let element of inputs) {
+        if(element.value && element.name){
+            configObj[element.name] = element.value;
+        }
+    }
+
+    localStorage.setItem(CONFIG_LOCAL_STORAGE_KEY, JSON.stringify(configObj));
+}
+
 function notify() {
-    var options = {
-        body: defaults.message,
+    const configObj = getConfigObj();
+
+    const options = {
+        body: configObj.body || defaults.body,
         icon: 'img/squirtle.png'
     }
   
-    var notification = new Notification(defaults.title, options);
-
-    setTimeout(notify, 1 * 60 * 60 * 1000);
+    const title = configObj.title || defaults.title;
+    const notification = new Notification(title, options);
+    const timeout = (configObj.minutesTimeout || defaults.minutesTimeout) * 60 * 1000;
+    setTimeout(notify, timeout);
 }
+
+const getConfigObj = () => JSON.parse(localStorage.getItem(CONFIG_LOCAL_STORAGE_KEY));
